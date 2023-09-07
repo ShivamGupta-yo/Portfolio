35e239cb34858433b01f97c7ff57a52c8fb36f8d
Shivam Gupta <112889717+GoldReigner@users.noreply.github.com>
2023-09-07 22:24:14 +0530

    1st final commit

--- portfolio.js
+++ portfolio.js
@@ -29,49 +29,40 @@ function opentab(tabname){
 
     // google sheets
     
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbzIFtSgeUyUbRgUijJnupV2xYahtPsxOZVS0CDdykNz83dw0TOeSNNnRRhzOATZLMW5/exec'
-  const form = document.forms['submit-to-google-sheet'];
-  const msg = document.getElementById('msg');
-  const submitButton = document.getElementById('submitButton'); // Add this line
-  
-  form.addEventListener('submit', e => {
-      e.preventDefault();
-  
-      // Show "Submitting..." on the submit button and disable it
-      submitButton.innerHTML = 'Submitting...';
-      submitButton.disabled = true;
-  
-      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-          .then(response => response.json())
-          .then(data => {
-              if (data) {
-                  msg.innerHTML = 'Message submitted successfully!!';
-                  form.reset();
-              }
-          })
-          .catch(error => {
-              console.error('Error!', error.message);
-              msg.innerHTML = 'An error occurred while submitting.';
-          })
-          .finally(() => {
-              // Revert the button text and enable it after the response
-              submitButton.innerHTML = 'Submit';
-              submitButton.disabled = false;
-          });
-  });
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzIFtSgeUyUbRgUijJnupV2xYahtPsxOZVS0CDdykNz83dw0TOeSNNnRRhzOATZLMW5/exec';
+    const form = document.forms['submit-to-google-sheet'];
+    const msg = document.getElementById('msg');
+    const submitButton = document.getElementById('submitButton');
+    
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+    
+        // Show "Submitting..." on the submit button and disable it
+        submitButton.innerHTML = 'Submitting...';
+        submitButton.disabled = true;
+        submitButton.classList.add('submitting-button'); // Add the blue styling class
+    
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => response.json())
+            .then(data => {
+                if (data) {
+                    msg.innerHTML = 'Message submitted successfully!!';
+                    form.reset();
+                }
+            })
+            .catch(error => {
+                console.error('Error!', error.message);
+                msg.innerHTML = 'An error occurred while submitting.';
+            })
+            .finally(() => {
+                // Revert the button text and enable it after the response
+                submitButton.innerHTML = 'Submit';
+                submitButton.disabled = false;
+                submitButton.classList.remove('submitting-button'); // Remove the violet styling class
+            });
+    });
+    
 
-  function doPost(e) {
-    try {
-      // Your code here
-      console.log("Request received successfully:", e);
-      // Process the data
-      // ...
-      return ContentService.createTextOutput(JSON.stringify({ success: true }));
-    } catch (error) {
-      console.error("Error:", error);
-      return ContentService.createTextOutput(JSON.stringify({ success: false, error: error.message }));
-    }
-  }
   
   
 
